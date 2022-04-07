9445c0e282fe456c99fcc36b7958b51ea4c4432a
Sema <romanov7semen@ya.ru>
2022-04-08 02:53:06 +0300

    dialog fragment/ dice random number

--- app/src/main/java/com/example/dnd/MainActivity.java
+++ app/src/main/java/com/example/dnd/MainActivity.java
@@ -63,9 +63,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SheetActivity.class);
-                intent.putExtra("json", "");
-                startActivityForResult(intent, 300);
+                CustomDialogFragment dialog = new CustomDialogFragment();
+                dialog.show(getSupportFragmentManager(), "custom");
+//                Intent intent = new Intent(MainActivity.this, SheetActivity.class);
+//                intent.putExtra("json", "");
+//                startActivityForResult(intent, 300);
             }
         });
         roomButton.setOnClickListener(new View.OnClickListener() {
