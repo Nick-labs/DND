451c5405d1caf3199ad5f26ceca33ee0d0320977
Sema <romanov7semen@ya.ru>
2022-05-06 00:15:48 +0300

    delete json file

--- app/src/main/java/com/example/dnd/SheetActivity.java
+++ app/src/main/java/com/example/dnd/SheetActivity.java
@@ -3,8 +3,10 @@ package com.example.dnd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
@@ -27,13 +29,17 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SheetActivity extends AppCompatActivity {
-    Button dice;
-    Button buttonBack;
-    EditText editTextName;
-    EditText editTextClass;
-    EditText editTextLevel;
+    Button buttonBack, dice, deleteButton;
+
+    EditText editTextName, editTextClass, editTextLevel, etKD, etIni, etSpeed, etHP, etTempHP, etHPDice, etSTR;
+    EditText etDEX, etCON, etINT, etWIS, etCHA, etAthletics, etSavSTR, etAcrobat, etHand, etStealth;
+    EditText etSavDEX, etSavCON, etAnaliz, etHist, etMag, etNature, etRelig, etSavINT, etVnimat;
+    EditText etSurv, etMed, etPronic, etAnim, etSavWIS, etPerf, etZapug, etLie, etUbezhd, etSavCHA;
+    EditText etInv, etProf, etFeatures;
 
     JSONObject json;
 
@@ -46,14 +52,70 @@ public class SheetActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().hide();
 
-        dice = findViewById(R.id.dice);
+        etKD = findViewById(R.id.etKD);
+        etIni = findViewById(R.id.etIni);
+        etSpeed = findViewById(R.id.etSpeed);
+        etHP = findViewById(R.id.etHP);
+        etTempHP = findViewById(R.id.etTempHP);
+        etHPDice = findViewById(R.id.etHPDice);
+        etSTR = findViewById(R.id.etSTR);
+        etAthletics = findViewById(R.id.etAthletics);
+        etSavSTR = findViewById(R.id.etSavSTR);
+        etDEX = findViewById(R.id.etDEX);
+        etAcrobat = findViewById(R.id.etAcrobat);
+        etHand = findViewById(R.id.etHand);
+        etStealth = findViewById(R.id.etStealth);
+        etSavDEX = findViewById(R.id.etSavDEX);
+        etCON = findViewById(R.id.etCON);
+        etSavCON = findViewById(R.id.etSavCON);
+        etINT = findViewById(R.id.etINT);
+        etAnaliz = findViewById(R.id.etAnaliz);
+        etHist = findViewById(R.id.etHist);
+        etMag = findViewById(R.id.etMag);
+        etNature = findViewById(R.id.etNature);
+        etRelig = findViewById(R.id.etRelig);
+        etSavINT = findViewById(R.id.etSavINT);
+        etWIS = findViewById(R.id.etWIS);
+        etVnimat = findViewById(R.id.etVnimat);
+        etSurv = findViewById(R.id.etSurv);
+        etMed = findViewById(R.id.etMed);
+        etPronic = findViewById(R.id.etPronic);
+        etAnim = findViewById(R.id.etAnim);
+        etSavWIS = findViewById(R.id.etSavWIS);
+        etCHA = findViewById(R.id.etCHA);
+        etPerf = findViewById(R.id.etPerf);
+        etUbezhd = findViewById(R.id.etUbezhd);
+        etZapug = findViewById(R.id.etZapug);
+        etLie = findViewById(R.id.etLie);
+        etSavCHA = findViewById(R.id.etSavCHA);
+
+
         buttonBack = findViewById(R.id.buttonBack);
+        deleteButton = findViewById(R.id.deleteButton);
+        dice = findViewById(R.id.dice);
         editTextName = findViewById(R.id.editTextName);
         editTextClass = findViewById(R.id.editTextClass);
         editTextLevel = findViewById(R.id.editTextLevel);
+        etInv = findViewById(R.id.editTextInv);
+        etProf = findViewById(R.id.editTextProf);
+        etFeatures = findViewById(R.id.editTextFeatures);
+
+
+
+ /*
+            вызов диалогового окна для кубиков
+        */
+        dice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogActivity(SheetActivity.this).show();
+            }
+        });
+
 
         Intent intent = getIntent();
         String jsonString = intent.getStringExtra("json");
+        String path = this.getFilesDir().toString();
         System.out.println(jsonString);
 
         if (!jsonString.equals("")) {
@@ -61,36 +123,155 @@ public class SheetActivity extends AppCompatActivity {
                 json = new JSONObject(jsonString);
                 editTextName.setText(json.getString("name"));
                 editTextClass.setText(json.getString("class"));
-                editTextLevel.setText(json.getInt("level") + "");
+                editTextLevel.setText(json.getString("level"));
+                etKD.setText(json.getString("KD"));
+                etIni.setText(json.getString("initiative"));
+                etSpeed.setText(json.getString("speed"));
+                etHP.setText(json.getString("HP"));
+                etTempHP.setText(json.getString("tempHP"));
+                etHPDice.setText(json.getString("HPDice"));
+                etSTR.setText(json.getString("STR"));
+                etAthletics.setText(json.getString("athletics"));
+                etSavSTR.setText(json.getString("savSTR"));
+                etDEX.setText(json.getString("DEX"));
+                etAcrobat.setText(json.getString("acrobatics"));
+                etHand.setText(json.getString("hand"));
+                etStealth.setText(json.getString("stealth"));
+                etSavDEX.setText(json.getString("savDEX"));
+                etCON.setText(json.getString("CON"));
+                etSavCON.setText(json.getString("savCON"));
+                etINT.setText(json.getString("INT"));
+                etAnaliz.setText(json.getString("analiz"));
+                etHist.setText(json.getString("hist"));
+                etMag.setText(json.getString("mag"));
+                etNature.setText(json.getString("nature"));
+                etRelig.setText(json.getString("relig"));
+                etSavINT.setText(json.getString("savINT"));
+                etWIS.setText(json.getString("WIS"));
+                etVnimat.setText(json.getString("vnimat"));
+                etSurv.setText(json.getString("surv"));
+                etMed.setText(json.getString("med"));
+                etPronic.setText(json.getString("pronic"));
+                etAnim.setText(json.getString("anim"));
+                etSavWIS.setText(json.getString("savWIS"));
+                etCHA.setText(json.getString("CHA"));
+                etPerf.setText(json.getString("perf"));
+                etUbezhd.setText(json.getString("zapug"));
+                etZapug.setText(json.getString("lie"));
+                etLie.setText(json.getString("ubezhd"));
+                etSavCHA.setText(json.getString("savCHA"));
+
+                etInv.setText(json.getString("inv"));
+                etProf.setText(json.getString("prof"));
+                etFeatures.setText(json.getString("features"));
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
 
-        /*
-            вызов диалогового окна для кубиков
-        */
-        dice.setOnClickListener(new View.OnClickListener() {
+        deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-               dialogActivity(SheetActivity.this).show();
+            public void onClick(View view) {
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(SheetActivity.this);
+                builder1.setTitle("Delete");
+                builder1.setMessage("Are you sure?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                try {
+                                    File file = new File(path + "/" +  json.getString("name") + ".json");
+                                    System.out.println("Delete " + file);
+                                    System.out.println(file.exists());
+                                    if (file.exists()) {
+                                        boolean deleted = file.delete();
+                                        if (deleted) {
+                                            Toast.makeText(getBaseContext(), "Удалено", Toast.LENGTH_LONG).show();
+                                        }
+    ;
+                                        System.out.println("Удалено");
+                                        finish();
+                                }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getBaseContext(), "Молодец, и не надо удалять, это плохо))))", Toast.LENGTH_LONG).show();
+                                dialog.cancel();
+                            }
+                        });
+
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
             }
         });
+
 
         buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 JSONObject pers1 = new JSONObject();
                 String name = editTextName.getText().toString();
-                if (!editTextLevel.getText().toString().equals("")){
+                if (!editTextLevel.getText().toString().equals("")) {
                     try {
                         pers1.put("name", name);
                         pers1.put("class", editTextClass.getText().toString());
-                        pers1.put("level", Integer.parseInt(editTextLevel.getText().toString()));
+                        pers1.put("level", editTextLevel.getText().toString());
+                        pers1.put("KD", etKD.getText().toString());
+                        pers1.put("initiative", etIni.getText().toString());
+                        pers1.put("speed", etSpeed.getText().toString());
+                        pers1.put("HP", etHP.getText().toString());
+                        pers1.put("tempHP", etTempHP.getText().toString());
+                        pers1.put("HPDice", etHPDice.getText().toString());
+                        pers1.put("STR", etSTR.getText().toString());
+                        pers1.put("athletics", etAthletics.getText().toString());
+                        pers1.put("savSTR", etSavSTR.getText().toString());
+                        pers1.put("DEX", etDEX.getText().toString());
+                        pers1.put("acrobatics", etAcrobat.getText().toString());
+                        pers1.put("hand", etHand.getText().toString());
+                        pers1.put("stealth", etStealth.getText().toString());
+                        pers1.put("savDEX", etSavDEX.getText().toString());
+                        pers1.put("CON", etCON.getText().toString());
+                        pers1.put("savCON", etSavCON.getText().toString());
+                        pers1.put("INT", etINT.getText().toString());
+                        pers1.put("analiz", etAnaliz.getText().toString());
+                        pers1.put("hist", etHist.getText().toString());
+                        pers1.put("mag", etMag.getText().toString());
+                        pers1.put("nature", etNature.getText().toString());
+                        pers1.put("relig", etRelig.getText().toString());
+                        pers1.put("savINT", etSavINT.getText().toString());
+                        pers1.put("WIS", etWIS.getText().toString());
+                        pers1.put("vnimat", etVnimat.getText().toString());
+                        pers1.put("surv", etSurv.getText().toString());
+                        pers1.put("med", etMed.getText().toString());
+                        pers1.put("pronic", etPronic.getText().toString());
+                        pers1.put("anim", etAnim.getText().toString());
+                        pers1.put("savWIS", etSavWIS.getText().toString());
+                        pers1.put("CHA", etCHA.getText().toString());
+                        pers1.put("perf", etPerf.getText().toString());
+                        pers1.put("zapug", etZapug.getText().toString());
+                        pers1.put("lie", etLie.getText().toString());
+                        pers1.put("ubezhd", etUbezhd.getText().toString());
+                        pers1.put("savCHA", etSavCHA.getText().toString());
+                        pers1.put("inv", etInv.getText().toString());
+                        pers1.put("prof", etProf.getText().toString());
+                        pers1.put("features", etFeatures.getText().toString());
 
                         String jsonStr = pers1.toString();
                         System.out.println("jsonString: " + jsonStr);
                         writeToFile(name + ".json", jsonStr, getBaseContext());
+                        Toast.makeText(getBaseContext(), "Успешно сохранено", Toast.LENGTH_LONG).show();
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
@@ -99,10 +280,20 @@ public class SheetActivity extends AppCompatActivity {
                 finish();
             }
         });
-
     }
 
-    private Dialog dialogActivity(Context context){
+    private void writeToFile(String fileName, String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            System.out.println("Created");
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+    private Dialog dialogActivity(Context context) {
         final Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.dialog_dice_2);
 
@@ -159,17 +350,4 @@ public class SheetActivity extends AppCompatActivity {
 
         return dialog;
     }
-
-    private void writeToFile(String fileName, String data, Context context) {
-        try {
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(fileName, Context.MODE_PRIVATE));
-            outputStreamWriter.write(data);
-            System.out.println("Created");
-            outputStreamWriter.close();
-        } catch (IOException e) {
-            Log.e("Exception", "File write failed: " + e.toString());
-        }
-    }
-
-
 }
