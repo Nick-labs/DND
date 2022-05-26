0911f80ce81bd54e9dbb802f01dcbae1d822a142
Sema <romanov7semen@ya.ru>
2022-05-26 22:16:00 +0300

    dnd site 1.0.1

---
+++ app/src/main/java/com/example/dnd1/DndСlub.java
@@ -0,0 +1,24 @@
+package com.example.dnd1;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DndСlub extends AppCompatActivity {
+    WebView wb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_dnd_club);
+        wb = findViewById(R.id.webBrowser);
+        WebSettings webSettings = wb.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        DndWebClient wbc = new DndWebClient(this);
+        wb.setWebViewClient(wbc);
+        wb.loadUrl("https://old.dnd5.club/");
+    }
+}
